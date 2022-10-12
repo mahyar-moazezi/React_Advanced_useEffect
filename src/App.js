5e0bf5a363f3028840e6923c1ebae7da5ff86b30
mahyar moazezi <mahyar.moazezi@gmail.com>
2022-10-12 06:25:32 +0000

    add useLatoutEffect to the hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState, useMemo } from "react";
 import "./styles.css";
 import { useFetch } from "./useFetch";
 export default function App() {
-  const { data } = useFetch({
-    url: "/joe.json",
-  });
+  const [url, setUrl] = useState(null);
+  // const myOptions = useMemo(() => ({ url }), [url]);
+  const { data } = useFetch({ url, onSuccess: () => console.log("sucess") });
+
+  console.log("App Rendering");
   return (
     <div className="App">
-      <div>Hi</div>
-      <div>{JSON.stringify(data)}</div>
+      <h1>Hello</h1>
+      <div>{data ? JSON.stringify(data) : "No Data"}</div>
+      <button onClick={() => setUrl("max.json")} style={{ margin: "20px" }}>
+        GET DATA
+      </button>
     </div>
   );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,12 +1,31 @@
-import React from "react";
-import { useEffect, useState } from "react/cjs/react.production.min";
-
+import { useEffect, useState, useRef, useLayoutEffect } from "react";
+const useCallbackRef = (callback) => {
+  const callbackRef = useRef(callback);
+  useLayoutEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+  return callbackRef;
+};
 export const useFetch = (options) => {
   const [data, setData] = useState(null);
+  const savedOnSuccess = useCallbackRef(options.onSuccess);
+
   useEffect(() => {
-    fetch(options.url)
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  },[options.url]);
-  return data;
+    console.log("useFetch useEffect");
+    if (options.url) {
+      let isCanceled = false;
+      fetch(options.url)
+        .then((response) => response.json())
+        .then((json) => {
+          if (!isCanceled) {
+            savedOnSuccess.current?.(json);
+            setData(json);
+          }
+        });
+      return () => {
+        isCanceled = true;
+      };
+    }
+  }, [options.url]);
+  return { data };
 };
